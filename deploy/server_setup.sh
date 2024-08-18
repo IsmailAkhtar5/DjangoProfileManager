ca774c6d43571b118ee72ca07980eccd784312ee
ismail akhtar <167802247+IsmailAkhtar5@users.noreply.github.com>
2024-08-18 18:07:37 +0500

    Update server_setup.sh

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -11,10 +11,10 @@ VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
+# Install Python, SQLite, and pip
 echo "Installing dependencies..."
 apt-get update
-apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
+apt-get install -y python3-dev python3-venv sqlite3 python3-pip supervisor nginx git
 
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/profiles-rest-api
